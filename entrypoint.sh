d6c217db3ed682aa6fb633990a1c20ae43bd88c2
Rakesh Tholiya <71389445+fit-rakesh@users.noreply.github.com>
2022-07-29 17:52:18 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,5 @@ export GITHUB="true"
 echo $INPUT_USERNAME
 echo $INPUT_HOST
 echo $INPUT_KEY >> key.pem
-echo $INPUT_KEY >> key.pem
 ssh -i key.pem $INPUT_USERNAME@$INPUT_HOST touch abc.text
 echo "end"
