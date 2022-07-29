414feaa9e4e18a9e4e634ce797bca8f019d51613
Rakesh Tholiya <71389445+fit-rakesh@users.noreply.github.com>
2022-07-29 17:58:54 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ echo $INPUT_HOST
 #echo $INPUT_KEY >> key.pem
 #ssh -i key.pem $INPUT_USERNAME@$INPUT_HOST touch abc.text
 eval $(ssh-agent -s)
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+echo "$INPUT_KEY" | tr -d '\r' | ssh-add - > /dev/null
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 apk add --update --no-cache sshpass openssh-client jq net-tools curl gettext
